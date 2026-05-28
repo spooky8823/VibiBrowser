8c60885e0a653d41c9629163ff55054f0c450f2a
Spooky <spookyminecraftgamer@gmail.com>
2026-05-28 13:47:28 +0530

    chromium engine updated to Chrome/136.0.0.0

--- src/webview.cpp
+++ src/webview.cpp
@@ -22,7 +22,7 @@ WebView::WebView(QWidget *parent) : QWebEngineView(parent)
     // Set a real user agent so Google doesn't captcha us
     page()->profile()->setHttpUserAgent(
         "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) "
-        "VibiBrowser/1.1.0 Chrome/120.0.0.0 Safari/537.36"
+        "VibiBrowser/1.1.0 Chrome/136.0.0.0 Safari/537.36"
     );
 
     connect(this, &QWebEngineView::urlChanged, this, [this](const QUrl &url){
@@ -38,12 +38,12 @@ WebView::WebView(QWidget *parent) : QWebEngineView(parent)
         {
             page()->profile()->setHttpUserAgent(
                 "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 "
-                "(KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+                "(KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36"
             );
         } else {
             page()->profile()->setHttpUserAgent(
                 "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 "
-                "(KHTML, like Gecko) VibiBrowser/1.1.0 Chrome/120.0.0.0 Safari/537.36"
+                "(KHTML, like Gecko) VibiBrowser/1.1.0 Chrome/136.0.0.0 Safari/537.36"
             );
         }
 
